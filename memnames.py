f587d89370e91180e6dc10e616bc6299b625450c
Paul Gowder <paul.gowder@gmail.com>
2015-08-10 00:20:41 -0500

    probably shouldn’t overwrite namespace of python builtins…

--- memnames.py
+++ memnames.py
@@ -6,19 +6,19 @@ import json, cgi, random, cgitb
 # after the problems are fixed in order to not horrifically break even the minimal security baked in here.
 # cgitb.enable()
 
-def checkPrev(id):
-    firstCheck = students[id]['Firstname'] == fname
-    lastCheck = students[id]['Lastname'] == lname
+def checkPrev(sid):
+    firstCheck = students[sid]['Firstname'] == fname
+    lastCheck = students[sid]['Lastname'] == lname
     return (firstCheck, lastCheck)
 
 def displayNew():
-    id = random.randint(1, len(students))
-    printStudent(id)
+    sid = random.randint(1, len(students))
+    printStudent(sid)
 
-def displaySkipped(id):
-    print "The last student was %s %s <br>" % (students[id]['Firstname'], students[id]['Lastname'])
+def displaySkipped(sid):
+    print "The last student was %s %s <br>" % (students[sid]['Firstname'], students[sid]['Lastname'])
     print "and here is his/her photo: <br>"
-    image = imgp + students[id]['Photo']
+    image = imgp + students[sid]['Photo']
     print "<img src=\"%s\"><br>" % image
     print "It's ok, here's another: <br>"
     displayNew()
@@ -28,35 +28,35 @@ def displayCorrect():
     print "Now here's another: <br>"
     displayNew()
 
-def displayFWrong(id):
-    print "You got the last name, %s, right.  Now let's get the first one, eh?<br>" % students[id]['Lastname']
-    printStudent(id)
+def displayFWrong(sid):
+    print "You got the last name, %s, right.  Now let's get the first one, eh?<br>" % students[sid]['Lastname']
+    printStudent(sid)
 
-def displayLWrong(id):
-    print "You got the first name, %s, right.  Now let's get the first one, eh?<br>" % students[id]['Firstname']
-    printStudent(id)
+def displayLWrong(sid):
+    print "You got the first name, %s, right.  Now let's get the first one, eh?<br>" % students[sid]['Firstname']
+    printStudent(sid)
 
-def displayWrong(id):
+def displayWrong(sid):
     print "Wrong.  Try again. <br>"
-    printStudent(id)
+    printStudent(sid)
 
 def headprint():
     print "Content-Type: text/html\n\n"
     print "<html><head><title>Student Flashcards</title></head><body>"
     print "<center>"
 
-def printStudent(id):
+def printStudent(sid):
     print "<br>"
-    image = imgp + students[id]['Photo']
+    image = imgp + students[sid]['Photo']
     print "<img src=\"%s\"><br>" % image
     print "<form action=\"memnames.cgi\" method=\"get\">"
     print "First Name: <input type=\"text\" name=\"fname\"><br>"
     print "Last Name: <input type=\"text\" name=\"lname\"><br>"
-    print "<input type=\"hidden\" name=\"id\" value=\"%s\">" % id
+    print "<input type=\"hidden\" name=\"id\" value=\"%s\">" % sid
     print "<input type=\"hidden\" name=\"pw\" value=\"%s\">" % password
     print "<input type=\"hidden\" name=\"file\" value=\"%s\">" % filename
     print "<input type=\"submit\" value=\"Submit\"><br>"
-    print "<a href=\"memnames.cgi?id=%s&skipped=yes&file=%s&pw=%s\">Skip</a>" % (id, filename, password)
+    print "<a href=\"memnames.cgi?id=%s&skipped=yes&file=%s&pw=%s\">Skip</a>" % (sid, filename, password)
     print "</center></body></html>"
 
 
@@ -82,13 +82,13 @@ except:
 if not password == students[0]['Password']:
     raise AuthenticationError('wrong password')
 
-id = None
+sid = None
 fname = None
 lname = None
 skipped = None
 
 try:
-    id = int(form.getvalue('id'))
+    sid = int(form.getvalue('id'))
     fname = form.getvalue('fname')
     lname = form.getvalue('lname')
 except KeyboardInterrupt:
@@ -97,7 +97,7 @@ except:
     pass
 
 try:
-    id = int(form.getvalue('id'))
+    sid = int(form.getvalue('id'))
     skipped = form.getvalue('skipped')
 except KeyboardInterrupt:
     raise
@@ -107,17 +107,17 @@ except:
 
 headprint()
 
-if id == None:
+if sid == None:
     displayNew()
 elif skipped == "yes":
-    displaySkipped(id)
+    displaySkipped(sid)
 else:
-    last = checkPrev(id)
+    last = checkPrev(sid)
     if last == (True, True):
         displayCorrect()
     elif last == (False, True):
-        displayFWrong(id)
+        displayFWrong(sid)
     elif last == (True, False):
-        displayLWrong(id)
+        displayLWrong(sid)
     else:
-        displayWrong(id)
+        displayWrong(sid)
